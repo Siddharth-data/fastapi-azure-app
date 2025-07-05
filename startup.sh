7964e9a09134a331eaaed6fd1b38ea9763494ae3
Siddharth Seshadri <siddharthseshadri@Siddharths-MacBook-Air.local>
2025-07-05 14:11:22 +0530

    fix startup issue

--- startup.sh
+++ startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Install required packages
 pip install -r requirements.txt
-
-# Start the FastAPI app from app/main.py with module path app.main:app
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn main:app --host 0.0.0.0 --port 8000
